8f6f01143aa18098160f923dca908273eee79e1b
Arfon Smith <arfon@github.com>
2014-02-11 15:52:05 -0600

    Empty Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Theoj::Application.config.secret_key_base = 'e381adaa5b291f4654a9f8e162c571886644676330df8bac30cf537a7a859f088c95ca2db3ba9c0c480d62b544d0547d27ce21a9228a606a2f8776a4be413d43'
