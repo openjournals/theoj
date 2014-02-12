f74a338a917876337e2eacb499b714afbb26a504
Arfon Smith <arfon@github.com>
2014-02-11 20:32:59 -0600

    Updating Schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,51 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140212012504) do
 
-  create_table "commontator_comments", force: true do |t|
-    t.string   "creator_type"
-    t.integer  "creator_id"
-    t.string   "editor_type"
-    t.integer  "editor_id"
-    t.integer  "thread_id",                      null: false
-    t.text     "body",                           null: false
-    t.datetime "deleted_at"
-    t.integer  "cached_votes_total", default: 0
-    t.integer  "cached_votes_up",    default: 0
-    t.integer  "cached_votes_down",  default: 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "commontator_comments", ["cached_votes_down"], name: "index_commontator_comments_on_cached_votes_down", using: :btree
-  add_index "commontator_comments", ["cached_votes_total"], name: "index_commontator_comments_on_cached_votes_total", using: :btree
-  add_index "commontator_comments", ["cached_votes_up"], name: "index_commontator_comments_on_cached_votes_up", using: :btree
-  add_index "commontator_comments", ["creator_id", "creator_type", "thread_id"], name: "index_c_c_on_c_id_and_c_type_and_t_id", using: :btree
-  add_index "commontator_comments", ["thread_id"], name: "index_commontator_comments_on_thread_id", using: :btree
-
-  create_table "commontator_subscriptions", force: true do |t|
-    t.string   "subscriber_type",             null: false
-    t.integer  "subscriber_id",               null: false
-    t.integer  "thread_id",                   null: false
-    t.integer  "unread",          default: 0, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "commontator_subscriptions", ["subscriber_id", "subscriber_type", "thread_id"], name: "index_c_s_on_s_id_and_s_type_and_t_id", unique: true, using: :btree
-  add_index "commontator_subscriptions", ["thread_id"], name: "index_commontator_subscriptions_on_thread_id", using: :btree
-
-  create_table "commontator_threads", force: true do |t|
-    t.string   "commontable_type"
-    t.integer  "commontable_id"
-    t.datetime "closed_at"
-    t.string   "closer_type"
-    t.integer  "closer_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "commontator_threads", ["commontable_id", "commontable_type"], name: "index_c_t_on_c_id_and_c_type", unique: true, using: :btree
-
   create_table "papers", force: true do |t|
     t.integer  "user_id"
     t.string   "location"
