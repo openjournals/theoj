d875fc40377632289a55e4f3534e4868ca8d8ab3
Stuart <stuart@zooniverse.org>
2014-05-06 21:46:22 -0500

    adding basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.destroy_all
+Paper.destroy_all
+
+users = []
+users << User.create(uid: 1, name: "stuart", extra: "blah blah", picture: "https://pbs.twimg.com/profile_images/2392941891/anq5bkkx8rtdhn8p2i9m_bigger.png")
+users << User.create(uid: 2, name: "arfon", extra: "blah blah", picture: "https://pbs.twimg.com/profile_images/2392941891/anq5bkkx8rtdhn8p2i9m_bigger.png")
+
+
+10.times do |no|
+  Paper.create( submitted_at: DateTime.now, title: "Paper no #{no}", location:"http://arxiv.org/abs/1405.103#{1+no}",user_id: users.sample.id )
+end
