a1f0f563001ca206674674ca46402f43fb85bbda
Stuart <stuart@zooniverse.org>
2014-06-15 16:30:40 -0500

    basic serialization for papers

---
+++ app/serializers/paper_serializer.rb
@@ -0,0 +1,17 @@
+class PaperSerializer < ActiveModel::Serializer
+  attributes :id, :user_permisions, :location, :state, :submitted_at, :title, :version, :created_at, :pending_issues_count
+  belongs_to :user
+
+  def user_permisions
+    if current_user
+      object.permisions_for_user current_user
+    else
+      []
+    end
+  end
+
+  def pending_issues_count
+    object.outstanding_issues.count
+  end
+
+end
