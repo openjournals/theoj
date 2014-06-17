1d4665b2ca14e392ad4cb36631ef16d36ad6c254
Stuart <stuart@zooniverse.org>
2014-06-17 12:06:57 -0500

    Not sure why the paper content is nested under user. None of the controllers actually use the user id to select paper types. This might be useful in future but without a nested route this should default to the current user scope just to make things easier

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Theoj::Application.routes.draw do
   resources :papers, defaults: { format: 'json' } do
     resources :annotations, defaults: { format: 'json' }
+    collection do
+      get :as_reviewer, defaults: { format: 'json' }
+      get :as_editor, defaults: { format: 'json' }
+      get :as_author, defaults: { format: 'json' }
+      get :as_collaborator, defaults: { format: 'json' }
+    end
   end
 
   get '/current_user', to:'users#get_current_user', defaults: {format: 'json'}
