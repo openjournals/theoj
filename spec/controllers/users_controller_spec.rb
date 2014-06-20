6783e71496b4ccc4f9f81d8f5c9103aab971e572
Arfon Smith <arfon@github.com>
2014-06-20 17:26:58 +0100

    Dumb response testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,21 @@ describe UsersController do
       get :show, :id => user.sha, :format => :json
       expect(response).to be_success
       expect(response.status).to eq(200)
-      expect(response.content_type).to eq("application/json") 
+      expect(response.content_type).to eq("application/json")
+    end
+  end
+end
+
+
+describe UsersController, '.get_current_user' do
+  describe "GET #get_current_user" do
+    it "responds successfully with an HTTP 200 status code" do
+      user = create(:user)
+      allow(controller).to receive_message_chain(:current_user).and_return(user)
+      get :get_current_user, :format => :json
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+      assert hash_from_json(response.body).has_key?("provider")
     end
   end
 end
