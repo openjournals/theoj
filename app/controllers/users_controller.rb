b39684d0eb02462a2bdee81703320230ddd9c03a
Stuart <stuart@zooniverse.org>
2014-06-24 17:26:48 -0500

    start of a method to look up users like github

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   respond_to :json
+
   def show
     user = User.find_by_sha(params[:id])
     render :json => user
@@ -7,5 +8,11 @@ class UsersController < ApplicationController
 
   def get_current_user
     respond_with current_user
+  end
+
+  def name_lookup
+    guess = params["guess"]
+    users = User.where("sha like ?", "%#{guess}%").select(:sha, :id).to_a
+    respond_with users
   end
 end
