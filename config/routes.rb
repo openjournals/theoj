3724a960d34f9aaaef9ec615ab085cd5328c8f17
Stuart <stuart@zooniverse.org>
2014-09-04 12:46:00 -0500

    updating endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -19,20 +19,22 @@ Theoj::Application.routes.draw do
 
   get '/current_user', to:'users#get_current_user', defaults: {format: 'json'}
 
-  resources :users, defaults: { format: 'json' }, only: [:show] do
-    resources :papers, defaults: { format: 'json' } do
-      collection do
-        get :as_reviewer, defaults: { format: 'json' }
-        get :as_editor, defaults: { format: 'json' }
-        get :as_author, defaults: { format: 'json' }
-        get :as_collaborator, defaults: { format: 'json' }
-      end
-    end
+  # resources :users, defaults: { format: 'json' }, only: [:show] do
+  #   resources :papers, defaults: { format: 'json' } do
+  #     collection do
+  #       get :as_reviewer, defaults: { format: 'json' }
+  #       get :as_editor, defaults: { format: 'json' }
+  #       get :as_author, defaults: { format: 'json' }
+  #       get :as_collaborator, defaults: { format: 'json' }
+  #     end
+  #   end
+  #
+  #   collection do
+  #     get :name_lookup, defaults: { format: 'json' }
+  #   end
+  # end
 
-    collection do
-      get :name_lookup, defaults: { format: 'json' }
-    end
-  end
+  get '/user/name_lookup', to: 'users#name_lookup'
 
   get '/sessions/new', to: 'sessions#new', as: 'new_session'
   get '/auth/:provider/callback', to: 'sessions#create'
