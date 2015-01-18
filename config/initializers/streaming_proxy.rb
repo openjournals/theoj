15a2148bcd6743be655d14e1959032ca19a94d08
Stuart <stuart@zooniverse.org>
2015-01-18 16:46:40 -0600

    default configuration for streaming proxy seems to work better than the pervious one

--- config/initializers/streaming_proxy.rb
+++ config/initializers/streaming_proxy.rb
@@ -1,11 +1,6 @@
 require 'rack/streaming_proxy'
 
 Theoj::Application.configure do
-  config.streaming_proxy.logger             = Rails.logger                          # stdout by default
-  config.streaming_proxy.log_verbosity      = Rails.env.production? ? :low : :high  # :low or :high, :low by default
-  config.streaming_proxy.num_retries_on_5xx = 5                                     # 0 by default
-  config.streaming_proxy.raise_on_5xx       = true                                  # false by default
-
   # Will be inserted at the end of the middleware stack by default.
   config.middleware.use Rack::StreamingProxy::Proxy do |request|
 
