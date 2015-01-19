6774467fa5f2c3e90b7d6c7ecbedce499a7b4697
Stuart <stuart@zooniverse.org>
2015-01-18 18:52:47 -0600

    asset fallback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Theoj::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -74,6 +74,7 @@ Theoj::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
+
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
