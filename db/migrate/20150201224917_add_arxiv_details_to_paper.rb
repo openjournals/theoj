d9adb62aab1da3d8b1a606421a617ba41a02c090
Stuart <stuart@zooniverse.org>
2015-02-02 11:19:26 -0600

    Text not string for summary

--- db/migrate/20150201224917_add_arxiv_details_to_paper.rb
+++ db/migrate/20150201224917_add_arxiv_details_to_paper.rb
@@ -1,7 +1,7 @@
 class AddArxivDetailsToPaper < ActiveRecord::Migration
   def change
     add_column :papers, :arxiv_id,     :string
-    add_column :papers, :summary,      :string
+    add_column :papers, :summary,      :text
     add_column :papers, :author_list,  :string
 
     add_index :papers, ["arxiv_id"]
