ee3e2a3338a556b809fcae8f5a752191d30e9ecc
Stuart <stuart@zooniverse.org>
2015-02-01 18:50:41 -0600

    updating create on papers controller to use the arxiv id seeing as this is going to be the primary way we reference papers. All other details are grabbed from the arXiv through the arXiv gem

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -113,6 +113,6 @@ class PapersController < ApplicationController
   private
 
   def paper_params
-    params.require(:paper).permit(:title, :location)
+    params.require(:paper).permit(:arxiv_id)
   end
 end
