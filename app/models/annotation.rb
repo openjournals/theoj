445f83c547dd9819c2dad833bfe93d8394e49ca9
Marc Rohloff <marc@marcrohloff.com>
2015-02-03 15:53:44 -0700

    Replace unsupported state_machine with aasm

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,21 +1,23 @@
 class Annotation < ActiveRecord::Base
+  include AASM
+
   belongs_to :paper
   belongs_to :user
 
   has_many :responses, :class_name => "Annotation", :foreign_key => "parent_id"
   belongs_to :parent, :class_name => "Annotation", :foreign_key => "parent_id"
 
-  scope :root_annotations , -> {where(:parent_id => nil)}
+  scope :root_annotations , -> { where(parent_id: nil) }
 
   validates_presence_of :body, :paper_id
 
-  state_machine :initial => :new do
-    state :new
+  aasm column: :state do
+    state :new,          initial:true
     state :resolved
     state :challenged
 
     event :resolve do
-      transition all => :resolved
+      transitions to: :resolved
     end
   end
 
