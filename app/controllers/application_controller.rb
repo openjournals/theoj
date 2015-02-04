ffc2fa931a7e85ec909bd8f1506ede4e630b7ba1
Stuart <stuart@zooniverse.org>
2015-02-03 21:03:52 -0600

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,6 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    @current_user = User.first
   end
   helper_method :current_user
 end
