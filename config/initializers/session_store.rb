b2f2f8480b08ea5feb776163ebddb7f452d3bfc0
Marc Rohloff <marc@marcrohloff.com>
2015-02-20 12:35:27 -0700

    Make login sessions last across sessions (2 weeks). Resolves #117

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Theoj::Application.config.session_store :cookie_store, key: '_theoj_session'
+Theoj::Application.config.session_store :cookie_store,
+                                        key:           '_theoj_session',
+                                        expire_after:  2.weeks,
+                                        http_only:     true
