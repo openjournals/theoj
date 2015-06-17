278e42cd4db15cfcdaed94bef5392fa81b81ef4b
Marc Rohloff <marc@marcrohloff.com>
2015-06-16 19:35:09 -0600

    Disable rspec option to  automatically use the documentation formatter for single files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,12 +26,12 @@ RSpec.configure do |config|
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
-  end
+  # if config.files_to_run.one?
+  #   # Use the documentation formatter for detailed output,
+  #   # unless a formatter has already been configured
+  #   # (e.g. via a command-line flag).
+  #   config.default_formatter = 'doc'
+  # end
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
