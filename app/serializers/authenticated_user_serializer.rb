c46b4b2ca6079590a0629a9d698a2854a633abc7
Marc Rohloff <marc@marcrohloff.com>
2015-06-22 15:50:42 -0600

    #165: Redirect to editing, review, papers or submit page on initial load.

--- app/serializers/authenticated_user_serializer.rb
+++ app/serializers/authenticated_user_serializer.rb
@@ -6,6 +6,21 @@ class AuthenticatedUserSerializer < ActiveModel::Serializer
              :picture,
              :sha,
              :editor,
-             :admin
+             :admin,
+             :has_papers_as_submittor,
+             :has_papers_as_reviewer,
+             :has_papers_as_editor
+
+  def has_papers_as_submittor
+    object.papers_as_submittor.any?
+  end
+
+  def has_papers_as_reviewer
+    object.assignments_as_reviewer.any?
+  end
+
+  def has_papers_as_editor
+    object.assignments_as_editor.any?
+  end
 
 end
--- spec/serializers/authenticated_user_serializer_spec.rb
+++ spec/serializers/authenticated_user_serializer_spec.rb
@@ -8,9 +8,10 @@ describe AuthenticatedUserSerializer do
     serializer = AuthenticatedUserSerializer.new(user)
     hash = hash_from_json(serializer.to_json)
 
-    ["name", "email", "created_at", "picture", "sha", "editor", "admin"].each do |key|
-      expect(hash).to include(key)
-    end
+    expect(hash.keys).to contain_exactly("name", "email",
+                                         "created_at", "picture", "sha",
+                                         "has_papers_as_submittor", "has_papers_as_reviewer", "has_papers_as_editor",
+                                         "editor", "admin")
   end
 
 end
